aed6a36fc3fe35eaac572b88c9cc1e7b969ddd14
mie <mie@Laptop.local>
2025-10-25 20:17:26 +0200

    edits to projects and technologies (added types

--- srcs/content/projects.ts
+++ srcs/content/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project[] = [
         id: 'youtubetrends',
         name: 'YoutubeTrends',
         description: 'Automated data pipeline for fetching YouTube trending videos and loading them into a database.',
-        technologies: ['python', 'docker', 'postgresql', 'airflow'],
+        technologies: ['python', 'docker', 'postgresql', 'airflow', 'git'],
         date: '2025-10-14',
         links: [
             {
@@ -19,7 +19,7 @@ export const projects: Project[] = [
         id: 'easyconfigs',
         name: 'EasyConfigs',
         description: 'Software that transforms the tedious process of manually writing configuration files into a visual, error-free experience.',
-        technologies: ['ts', 'docker', 'html', 'css', 'json', 'md'],
+        technologies: ['ts', 'docker', 'html', 'css', 'json', 'md', 'git'],
         date: '2025-08-09',
         links: [
             {
@@ -37,7 +37,7 @@ export const projects: Project[] = [
         id: 'inception',
         name: 'Inception',
         description: 'Docker-based web server application, comtains wordpress, mariadb, and nginx. Optimized for deployment on a VM.',
-        technologies: ['docker', 'nginx', 'mariadb', 'sh'],
+        technologies: ['docker', 'nginx', 'mariadb', 'sh', 'git'],
         date: '2025-02-05',
         links: [
             {
@@ -51,7 +51,7 @@ export const projects: Project[] = [
         id: 'trancendance',
         name: 'Trancendance',
         description: 'Simple one page web application for playing pong, with user authentication, tournaments, game settings and multiplayer functionality.',
-        technologies: ['python', 'docker', 'js', 'html', 'css', 'json'],
+        technologies: ['python', 'docker', 'js', 'html', 'css', 'json', 'git'],
         date: '2025-02-03',
         links: [
             {
@@ -69,7 +69,7 @@ export const projects: Project[] = [
         id: 'survive',
         name: 'Survive',
         description: '2D space survival game made with C++ and SFML, where the player must fight alien ships to survive as long as possible. This was my submission for the Supercell Coding Challenge 2024.',
-        technologies: ['cpp', 'cmake', 'sfml'],
+        technologies: ['cpp', 'cmake', 'sfml', 'git'],
         date: '2024-11-11',
         links: [
             {
@@ -83,7 +83,7 @@ export const projects: Project[] = [
         id: 'cub3d',
         name: 'Cub3D',
         description: '3D Raycasting game written in C, based on 2d maps and inspired by the classic game Wolfenstein 3D.',
-        technologies: ['c'],
+        technologies: ['c', 'git'],
         date: '2024-10-29',
         links: [
             {
@@ -97,7 +97,7 @@ export const projects: Project[] = [
         id: 'minishell',
         name: 'Minishell',
         description: 'Minishell is a recreation of bash, designed to help you understand how command-line interpreters work.',
-        technologies: ['c', 'sh'],
+        technologies: ['c', 'sh', 'git'],
         date: '2024-09-19',
         links: [
             {
@@ -111,7 +111,7 @@ export const projects: Project[] = [
         id: 'solong',
         name: 'so-long',
         description: 'Simple 2D game written in C using the MinilibX library, where the player controls a character to collect items and reach the exit while avoiding enemies.',
-        technologies: ['c'],
+        technologies: ['c', 'git'],
         date: '2024-03-22',
         links: [
             {
--- srcs/content/technologies.ts
+++ srcs/content/technologies.ts
@@ -1,108 +1,134 @@
 export const tech = {
     c: {
         name: 'C',
+        type: 'language',
         icon: 'c'
     },
     cpp: {
         name: 'C++',
+        type: 'language',
         icon: 'cplusplus'
     },
     python: {
         name: 'Python',
+        type: 'language',
         icon: 'python'
     },
     ts: {
         name: 'TypeScript',
+        type: 'language',
         icon: 'typescript'
     },
     js: {
         name: 'JavaScript',
+        type: 'language',
         icon: 'javascript'
     },
     html: {
         name: 'HTML',
+        type: 'language',
         icon: 'html5'
     },
     css: {
         name: 'CSS',
+        type: 'language',
         icon: 'css'
     },
     java: {
         name: 'Java',
+        type: 'language',
         icon: 'java'
     },
     docker: {
         name: 'Docker',
+        type: 'tool',
         icon: 'docker'
     },
     airflow: {
         name: 'Apache Airflow',
+        type: 'tool',
         icon: 'apacheairflow'
     },
     postgresql: {
         name: 'PostgreSQL',
+        type: 'tool',
         icon: 'postgresql'
     },
     md: {
         name: 'Markdown',
+        type: 'data-format',
         icon: 'markdown'
     },
     json: {
         name: 'JSON',
+        type: 'data-format',
         icon: 'json'
     },
     yaml: {
         name: 'YAML',
+        type: 'data-format',
         icon: 'yaml'
     },
     sfml: {
         name: 'SFML',
+        type: 'library',
         icon: 'sfml'
     },
     cmake: {
         name: 'CMake',
+        type: 'tool',
         icon: 'cmake'
     },
     sh: {
         name: 'Shell',
+        type: 'language',
         icon: 'shell'
     },
     mariadb: {
         name: 'MariaDB',
+        type: 'tool',
         icon: 'mariadb'
     },
     nginx: {
         name: 'NGINX',
+        type: 'tool',
         icon: 'nginx'
     },
     wp: {
         name: 'WordPress',
+        type: 'library',
         icon: 'wordpress'
     },
     git: {
         name: 'git',
+        type: 'tool',
         icon: 'git'
     },
     linux: {
         name: 'Linux',
+        type: 'tool',
         icon: 'linux'
     },
     figma: {
         name: 'Figma',
+        type: 'tool',
         icon: 'figma'
     },
     blender: {
         name: 'Blender',
+        type: 'tool',
         icon: 'blender'
     },
     react: {
         name: 'React',
+        type: 'library',
         icon: 'react'
     },
     node: {
         name: 'node.js',
+        type: 'library',
         icon: 'nodedotjs'
-    },
+    }
 }
 
 // NOTE FOR SELF: when adding a new technology, check link below first:
